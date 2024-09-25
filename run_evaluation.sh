76be7429691f274d7663885ea91894e956c38648
jeffrey <jsheng26@amherst.edu>
2024-09-24 22:01:33 -0400

    added minidev evaluations (untested)

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -9,10 +9,14 @@ mode_gt='gt'
 mode_predict='gpt'
 
 echo '''starting to compare with knowledge for ex and ves'''
-python3 -u ./evaluation/evaluation.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path_kg} --data_mode ${data_mode} \
+python3 -u ./evaluation/evaluation_ex.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path_kg} --data_mode ${data_mode} \
 --ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus} --mode_gt ${mode_gt} --mode_predict ${mode_predict} \
 --diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out} 
 
 python3 -u ./evaluation/evaluation_ves.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path_kg} --data_mode ${data_mode} \
+--ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus} --mode_gt ${mode_gt} --mode_predict ${mode_predict} \
+--diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out} 
+
+python3 -u ./evaluation/evaluation_f1.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path_kg} --data_mode ${data_mode} \
 --ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus} --mode_gt ${mode_gt} --mode_predict ${mode_predict} \
 --diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out} 
