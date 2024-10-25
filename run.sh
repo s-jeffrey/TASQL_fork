ab2cb273149268aaa1f14b1f2d06f079df79fbe4
jeffrey <jeffrey.shengofficial@gmail.com>
2024-10-25 04:20:08 -0400

    Adapt run.py for single query

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ db_root_path='./data/dev_databases'
 mode='mini_dev_sqlite'
 meaning_output_path='./outputs/column_meaning.json' 
 sql_output_path='./outputs/predict_dev.json'
+question_id=$1
 
 #As stated in Appendix A.1, we first generate a succinct description for each column.
 #You can comment out the following code and directly utilize './outputs/column_meaning.json' to bypass this step
@@ -11,6 +12,6 @@ sql_output_path='./outputs/predict_dev.json'
 # echo 'Description generation is finished.'
 
 echo 'Generate SQLs.'
-python3 ./run.py --db_root_path ${db_root_path} --mode ${mode} --column_meaning_path ${meaning_output_path} --output_path ${sql_output_path}
+python3 ./run.py --question_id ${question_id} --db_root_path ${db_root_path} --mode ${mode} --column_meaning_path ${meaning_output_path} --output_path ${sql_output_path}
 echo 'SQL generation is finished.'
 
